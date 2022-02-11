7e2b2bd88790726a7819e070846017920625bc68
Mnstanchev <79105107+Mnstanchev@users.noreply.github.com>
2022-02-11 11:06:13 +0200

    Adding the attack button

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,14 @@
 const ATTACK_VALUE = 10; // Global value
 let choosenMaxLife = 100;
 
+let currentMontsterHealth = choosenMaxLife;
+let currentPlayerHealth = choosenMaxLife;
+
 adjustHealthBars(choosenMaxLife);
+
+function attackHandler() {
+  const damage = dealMonsterDamage(ATTACK_VALUE);
+  currentMontsterHealth -= damage;
+}
+
+attackBtn.addEventListener('click', attackHandler);
