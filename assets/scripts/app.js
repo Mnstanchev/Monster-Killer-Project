5ae6c715fc7a03a012bb3aeff6c821d436c36d78
Mnstanchev <79105107+Mnstanchev@users.noreply.github.com>
2022-02-11 11:21:01 +0200

    Checking the Win-Condition

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 10; // Global value
+const MONSTER_ATTACK_VALUE = 14; // Global value
 let choosenMaxLife = 100;
 
 let currentMontsterHealth = choosenMaxLife;
@@ -9,6 +10,15 @@ adjustHealthBars(choosenMaxLife);
 function attackHandler() {
   const damage = dealMonsterDamage(ATTACK_VALUE);
   currentMontsterHealth -= damage;
+  const playerDemage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= playerDemage;
+  if (currentMontsterHealth <= 0 && currentPlayerHealth > 0) {
+    alert('Hurray! You`ve won!');
+  } else if (currentPlayerHealth <= 0 && currentMontsterHealth > 0) {
+    alert('Boomer! You lost!');
+  } else if (currentPlayerHealth <= 0 && currentMontsterHealth <= 0) {
+    alert('It`s a draw! Re-play again!');
+  }
 }
 
 attackBtn.addEventListener('click', attackHandler);
