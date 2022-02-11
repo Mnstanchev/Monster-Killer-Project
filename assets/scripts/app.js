b8c40f963e2928b720c6ea2435a81ef7abc37c39
Mnstanchev <79105107+Mnstanchev@users.noreply.github.com>
2022-02-11 11:26:11 +0200

    Adding  A "Strong Attack" Functionality

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,6 @@
 const ATTACK_VALUE = 10; // Global value
 const MONSTER_ATTACK_VALUE = 14; // Global value
+const STRONG_ATTACK_VALUE = 17; // Global value
 let choosenMaxLife = 100;
 
 let currentMontsterHealth = choosenMaxLife;
@@ -8,6 +9,20 @@ let currentPlayerHealth = choosenMaxLife;
 adjustHealthBars(choosenMaxLife);
 
 function attackHandler() {
+  const damage = dealMonsterDamage(STRONG_ATTACK_VALUE);
+  currentMontsterHealth -= damage;
+  const playerDemage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= playerDemage;
+  if (currentMontsterHealth <= 0 && currentPlayerHealth > 0) {
+    alert('Hurray! You`ve won!');
+  } else if (currentPlayerHealth <= 0 && currentMontsterHealth > 0) {
+    alert('Boomer! You lost!');
+  } else if (currentPlayerHealth <= 0 && currentMontsterHealth <= 0) {
+    alert('It`s a draw! Re-play again!');
+  }
+}
+
+function strongAttackHandler() {
   const damage = dealMonsterDamage(ATTACK_VALUE);
   currentMontsterHealth -= damage;
   const playerDemage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
@@ -22,3 +37,4 @@ function attackHandler() {
 }
 
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
